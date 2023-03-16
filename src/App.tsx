f7e0c4627515ca66ef9ebf79a37f7cabd89bc419
JaspalSingh1998 <jaspalsinghpersonal9@gmail.com>
2023-03-16 12:23:18 -0400

    setup a base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import { Button } from "@chakra-ui/react"
+import { Button, Grid, GridItem, Show } from "@chakra-ui/react"
 
 function App() {
-  return <Button colorScheme='blue'>Click Me</Button>
+  return (
+    <Grid gridTemplateAreas={{
+      base: `"nav" "main"`,
+      lg: `"nav nav" "aside main"`
+    }}>
+      <GridItem area='nav' bg="coral">Nav</GridItem>
+      <Show above="lg">
+        <GridItem area='aside' bg="orange">Aside</GridItem>
+      </Show>
+      <GridItem area='main' bg="red">main</GridItem>
+    </Grid>
+  )
 }
 
 export default App
