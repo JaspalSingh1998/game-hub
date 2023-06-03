0e9abeb038df309e12128160ffeb05aff5166366
JaspalSingh1998 <jaspalsinghpersonal9@gmail.com>
2023-06-03 13:29:57 -0400

    Display Genres

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
 import { Button, Grid, GridItem, Show } from "@chakra-ui/react"
 import GameGrid from "./components/GameGrid"
+import GenreList from "./components/GenreList"
 import Navbar from "./components/Navbar"
 
 function App() {
   return (
-    <Grid gridTemplateAreas={{
+    <Grid templateAreas={{
       base: `"nav" "main"`,
       lg: `"nav nav" "aside main"`
-    }}>
+    }}
+    templateColumns={{
+      base: '1fr',
+      lg: '200px 1fr'
+    }}
+    >
       <GridItem area='nav'>
         <Navbar />
       </GridItem>
       <Show above="lg">
-        <GridItem area='aside'>Aside</GridItem>
+        <GridItem area='aside' paddingX={5}>
+          <GenreList />
+        </GridItem>
       </Show>
       <GridItem area='main'>
         <GameGrid />
